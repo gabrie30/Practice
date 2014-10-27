ca9e280049ea77562cce80cb5bcf934fc9a44d52
Jay <jgabriels30@gmail.com>
2014-10-26 20:56:20 -0400

    initial commit

---
+++ substrings.rb
@@ -0,0 +1,29 @@
+#Implement a method 
+#substrings that takes a word as the first argument and then an array of valid 
+#substrings (your dictionary) as the second argument. It should return a hash listing each substring 
+#case insensitive that was found in the original string and how many times it was found.
+
+
+
+def substrings(string, dictionary)
+	current_word = string.downcase.split(" ")
+	hash_count = Hash.new(0)
+
+
+	dictionary.each do |search|
+		current_word.each do |word|
+		  if word.include?(search)
+			  hash_count[word] += 1
+		  end
+		end
+	end
+
+p hash_count
+
+end
+
+
+
+dictionary = %w[welcome to the jungle baby book your gunna die welcome to the jungle we got fun and games]
+
+substrings("Welcome to the jungle book, baby", dictionary)
