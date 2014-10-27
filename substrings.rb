f46f1d21fe4dd281481066e519100eba5030ed6a
Jay <jgabriels30@gmail.com>
2014-10-26 22:01:16 -0400

    changed string

--- substrings.rb
+++ substrings.rb
@@ -2,14 +2,14 @@
 #substrings that takes a word as the first argument and then an array of valid 
 #substrings (your dictionary) as the second argument. It should return a hash listing each substring 
 #case insensitive that was found in the original string and how many times it was found.
-
-
-
+ 
+ 
+ 
 def substrings(string, dictionary)
 	current_word = string.downcase.split(" ")
 	hash_count = Hash.new(0)
-
-
+ 
+ 
 	dictionary.each do |search|
 		current_word.each do |word|
 		  if word.include?(search)
@@ -17,13 +17,13 @@ def substrings(string, dictionary)
 		  end
 		end
 	end
-
+ 
 p hash_count
-
+ 
 end
-
-
-
-dictionary = %w[welcome to the jungle baby book your gunna die welcome to the jungle we got fun and games]
-
-substrings("Welcome to the jungle book, baby", dictionary)
+ 
+ 
+ 
+dictionary = %w[welcome the book and games ]
+ 
+substrings("Welcome to the jungle, we got fun and games.", dictionary)
