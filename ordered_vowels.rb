bafe51b66d3c6e1a12ab318c9a47aaa4624c8bac
gabrie30 <jgabriels30@gmail.com>
2014-11-22 11:50:54 -0500

    Update ordered_vowels.rb

--- ordered_vowels.rb
+++ ordered_vowels.rb
@@ -2,7 +2,7 @@
 # lowercase words and returns a string with just the words containing
 # all their vowels (excluding "y") in alphabetical order. Vowels may
 # be repeated (`"afoot"` is an ordered vowel word).
-#
+# 
 # You will probably want a helper method, `ordered_vowel_word?(word)`
 # which returns true/false if a word's vowels are ordered.
 
