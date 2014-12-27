264a777507c903f87cb6f3df7a23893a94ea99e0
jgabez <jgabriels30@gmail.com>
2014-12-26 22:39:04 -0500

    initial commit

---
+++ App Academy Practice Problems/08-two-sum.rb
@@ -0,0 +1,30 @@
+# Write a method that takes an array of numbers. If a pair of numbers
+# in the array sums to zero, return the positions of those two numbers.
+# If no pair of numbers sums to zero, return `nil`.
+#
+# Difficulty: medium.
+
+def two_sum(nums)
+  i = 0
+  while i < nums.length
+    j = 0
+      while j < nums.length
+        if nums[i] + nums[j] == 0
+          return [i, j]
+        end
+        j += 1
+      end
+    i += 1
+  end
+  nil
+end
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+puts(
+  'two_sum([1, 3, 5, -3]) == [1, 3]: ' + (two_sum([1, 3, 5, -3]) == [1, 3]).to_s
+)
+puts(
+  'two_sum([1, 3, 5]) == nil: ' + (two_sum([1, 3, 5]) == nil).to_s
+)
